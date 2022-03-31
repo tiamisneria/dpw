c24b9580eae433d159a381cce94bfa3ab727c5de
unknown <tia.misneria6242@student.unri.ac.id>
2022-03-31 04:24:24 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log ("Halo");
+const a = document.querySelectorAll("a");
+for (let i=0; i<a.length; i++){
+a[i].style.color= "salmon";
+a[i].style.fontFamily= "Lucida Calligraphy";
+a[i].style.fontSize= "14px";
+}
+var judul = document.querySelector(".heading1");
+judul.innerHTML="hallo!!!";
+judul.style.fontSize="50px";
+judul.style.fontFamily="Courier New";
+judul.style.marginBottom= "20px";
+judul.style.color= "salmon";
+
+var judul = document.querySelector(".heading3");
+judul.innerHTML=" Hello guys. saya Tia Misneria Saragih. I'm an informatics engineering student.ini kali pertama saya dalam membuat website, saya berharap kiranya website ini nantinya dapat membantu serta memberikan pengalaman yang baik bagi teman teman semua,terima kasih!!!";
+judul.style.fontSize="20px";
+judul.style.fontFamily="";
+judul.style.marginBottom= "20px";
+judul.style.color= "orange";
+
+var judul = document.querySelector("P")
+judul.style.fontSize="16px";
+judul.style.fontFamily="";
+judul.style.marginBottom= "2px";
+judul.style.color= "orange";
+
+const card = document.getElementsByClassName("konten");
+card[0].style.backgroundColor="green";
+card[1].style.backgroundColor="green";
+
+const button = document.getElementsByClassName("konten");
+button.style.backgroundColor= "yellow";
+button.style.backgroundColor= "yellow";
+
